65c1cb020f4f86269ae61d933a2f0a7f3322f9a6
Mujahid Naqvi <mujahid-naqvi@Mujahids-MacBook-Pro.local>
2023-03-02 15:22:40 +0500

    new routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('', function () {
+    return redirect('about');
 });
+
+Route::view("about","/about");
+
+Route::view('contact','/contact');
