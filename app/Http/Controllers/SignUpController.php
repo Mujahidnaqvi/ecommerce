24b10ba8bc36cfd41ea31d4a6bc7e2add43281ed
Mujahid Naqvi <mnshahg5@gmail.com>
2023-03-15 19:15:13 +0500

    http requests

--- app/Http/Controllers/SignUpController.php
+++ app/Http/Controllers/SignUpController.php
@@ -7,4 +7,19 @@ use Illuminate\Http\Request;
 class SignUpController extends Controller
 {
     //
+    function create(){
+
+    }
+
+    function read(){
+
+    }
+    
+    function delete(){
+
+    }
+
+    function update(){
+
+    }
 }
