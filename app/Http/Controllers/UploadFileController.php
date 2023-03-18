5101ede66b24762a10a10eebbd62ecf0a3137a86
Mujahid Naqvi <mnshahg5@gmail.com>
2023-03-18 09:37:26 +0500

    upload file

--- app/Http/Controllers/UploadFileController.php
+++ app/Http/Controllers/UploadFileController.php
@@ -10,6 +10,7 @@ class UploadFileController extends Controller
     function uploadFile(Request $req){
         //  echo "working";
         $data = $req->file('ufile')->store('public/storage');
+        // return $data;
         $req->session()->flash('uploaded',$data);
         return redirect('uploadfile');
     }
